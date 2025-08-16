d6857bd2298ded073a98e833e74961d537bd72ee
unknown <manviraghu357@gmail.com>
2025-08-16 13:32:24 +0530

    minor bug fix

--- bloomify/feedback/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'feedback'
-
-urlpatterns = [
-    path('view/<int:quiz_id>/', views.view_feedback, name='view_feedback'),
-    path('generate/<int:quiz_id>/', views.generate_feedback, name='generate_feedback'),
-]
